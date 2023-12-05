b08312e4779688aaf86ac70afbed405618e75b06
supa_trupa <supatruparus@gmail.com>
2023-12-05 20:18:06 +0500

    select all

--- lib/widgets/utils/utils.dart
+++ lib/widgets/utils/utils.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 extension SelectAll on TextEditingController {
   selectAll() {
-    selection = const TextSelection(baseOffset: 0, extentOffset: 99999);
+    selection = TextSelection(baseOffset: 0, extentOffset: text.length);
   }
 }
